e2c59994cc7b4800bfe78210b1e9d4e4cee272e3
Gatsu-7 <satysat03@gmail.com>
2024-07-30 14:52:33 +0530

    updated environment variables

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,16 +1,108 @@
+// import React, { useState } from "react";
+// import axios from "axios";
+// import "./Donate.css";
+
+// // const API_URL = process.env.REACT_APP_API_URL;
+
+// const Donate = () => {
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     email: "",
+//     location: "",
+//     items: "",
+//   });
+
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     axios
+//       .post(`http://localhost:5000/api/createuser`, formData)
+//       .then((response) => {
+//         console.log(response.data);
+//         alert("Donation submitted successfully!");
+//         setFormData({
+//           name: "",
+//           email: "",
+//           location: "",
+//           items: "",
+//         });
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//         alert("Failed to submit donation");
+//       });
+//   };
+
+//   return (
+//     <div className="donate-container">
+//       <h1 className="donate-title">Donate Your Clothes</h1>
+//       <p className="donate-description">
+//         Help others by donating your used clothes.
+//       </p>
+//       <form className="donate-form" onSubmit={handleSubmit}>
+//         <label className="donate-label">Name:</label>
+//         <input
+//           type="text"
+//           name="name"
+//           className="donate-input"
+//           value={formData.name}
+//           onChange={handleChange}
+//           required
+//         />
+
+//         <label className="donate-label">Email:</label>
+//         <input
+//           type="email"
+//           name="email"
+//           className="donate-input"
+//           value={formData.email}
+//           onChange={handleChange}
+//           required
+//         />
+
+//         <label className="donate-label">Location:</label>
+//         <textarea
+//           name="location"
+//           className="donate-input"
+//           value={formData.location}
+//           onChange={handleChange}
+//           required
+//         ></textarea>
+
+//         <label className="donate-label">Items:</label>
+//         <textarea
+//           name="items"
+//           className="donate-input"
+//           value={formData.items}
+//           onChange={handleChange}
+//           required
+//         ></textarea>
+
+//         <button type="submit" className="donate-button">
+//           Donate
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Donate;
+
 import React, { useState } from "react";
-import axios from "axios";
 import "./Donate.css";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 const Donate = () => {
-  const initialFormData = {
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     location: "",
     items: "",
-  };
-
-  const [formData, setFormData] = useState(initialFormData);
+  });
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -18,12 +110,23 @@ const Donate = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios
-      .post("http://localhost:5000/api/createuser", formData)
-      .then((response) => {
-        console.log(response.data);
+    fetch(`${API_URL}api/createuser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
         alert("Donation submitted successfully!");
-        setFormData(initialFormData); // Reset form fields
+        setFormData({
+          name: "",
+          email: "",
+          location: "",
+          items: "",
+        });
       })
       .catch((error) => {
         console.error(error);
@@ -41,8 +144,8 @@ const Donate = () => {
         <label className="donate-label">Name:</label>
         <input
           type="text"
-          className="donate-input"
           name="name"
+          className="donate-input"
           value={formData.name}
           onChange={handleChange}
           required
@@ -51,8 +154,8 @@ const Donate = () => {
         <label className="donate-label">Email:</label>
         <input
           type="email"
-          className="donate-input"
           name="email"
+          className="donate-input"
           value={formData.email}
           onChange={handleChange}
           required
@@ -60,17 +163,17 @@ const Donate = () => {
 
         <label className="donate-label">Location:</label>
         <textarea
-          className="donate-input"
           name="location"
+          className="donate-input"
           value={formData.location}
           onChange={handleChange}
           required
         ></textarea>
 
-        <label className="donate-label">Number of Clothing Items:</label>
+        <label className="donate-label">Items:</label>
         <textarea
-          className="donate-input"
           name="items"
+          className="donate-input"
           value={formData.items}
           onChange={handleChange}
           required
